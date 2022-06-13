81b06fe9b141e02a7765489a661871359cc187e1
Fuki Hayashi <knightf884@yahoo.co.jp>
2022-06-13 12:05:45 +0900

    レイアウトのテンプレート化

--- platform/router/router.go
+++ platform/router/router.go
@@ -14,6 +14,7 @@ import (
 	"lifelog/controllers/login"
 	"lifelog/controllers/logout"
 	"lifelog/platform/authenticator"
+	"lifelog/platform/render"
 )
 
 // New はroutesを登録し、ルーターを返す
@@ -26,7 +27,9 @@ func New(auth *authenticator.Authenticator) *gin.Engine {
 	router.Use(sessions.Sessions("auth-session", store))
 
 	// HTMLテンプレートパス設定
-	router.LoadHTMLGlob("views/*")
+	//router.LoadHTMLGlob("views/*")
+	router.HTMLRender = render.CreateRenderTemplates()
+	//	router.SetHTMLTemplate(render.RenderTemplate("home.html"))
 
 	router.Static("./assets", "./assets")
 
