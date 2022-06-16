92e6c6c4bbb99a7cdc4965e53f068401557978a8
Fuki Hayashi <knightf884@yahoo.co.jp>
2022-06-16 09:24:07 +0900

    記録の日付入力制限を追加（前後１ヶ月）

--- assets/javascripts/lifelog_new.js
+++ assets/javascripts/lifelog_new.js
@@ -1,8 +1,34 @@
 $(function() {
-    $('.js_timepic').datetimepicker({
+    var date = new Date();
+    $('#js_timepic_start').datetimepicker({
         step:10,
         dateformat: 'yyyy-mm-dd',
         timeformat: 'hh:mm',
+        minDate: date.getFullYear() + '/' + date.getMonth() + '/01',
+        maxDate: date.getFullYear() + '/' + (date.getMonth()+3) + '/01'
+    });
+});
+
+$(function(){
+    var date = new Date($('#js_timepic_start').val())
+    $('#js_timepic_start').change(function(){
+        date = new Date($('#js_timepic_start').val());
+        date.setMinutes(date.getMinutes()+30);
+        $('#js_timepic_end').val(date.getFullYear() + '/' + (date.getMonth()+1).toString().padStart(2, '0') + '/' + (date.getDate()).toString().padStart(2, '0') + ' ' + date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0'));
+        $('#js_timepic_end').datetimepicker({
+            step:10,
+            dateformat: 'yyyy-mm-dd',
+            timeformat: 'hh:mm',
+            minDate: $('#js_timepic_start').val(),
+            maxDate: date.getFullYear() + '/' + (date.getMonth()+1) + '/' + (date.getDate()+1)
+        });
+    });
+    $('#js_timepic_end').datetimepicker({
+        step:10,
+        dateformat: 'yyyy-mm-dd',
+        timeformat: 'hh:mm',
+        minDate: $('#js_timepic_start').val(),
+        maxDate: date.getFullYear() + '/' + (date.getMonth()+1) + '/' + (date.getDate()+1)
     });
 });
 
