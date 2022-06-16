0f2bebef2de30c26bd9cf7fe831ff051d7820172
Fuki Hayashi <knightf884@yahoo.co.jp>
2022-06-16 09:23:03 +0900

    備考の日付フォーマットが違うバグ修正

--- assets/javascripts/remarks_new.js
+++ assets/javascripts/remarks_new.js
@@ -1,6 +1,6 @@
 $(function() {
     $('.js_datepic').datetimepicker({
         timepicker:false, // 日付のみ表示
-        format:'Y-m-d', // フォーマットの指定
+        format:'yyyy-mm-dd', // フォーマットの指定
     });
 });
