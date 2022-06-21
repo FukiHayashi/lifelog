baabcc618b058532aa21ff7dc10ddb70698e82d3
Fuki Hayashi <knightf884@yahoo.co.jp>
2022-06-21 14:01:45 +0900

    envファイルを切り替えられるように修正

--- database/database.go
+++ database/database.go
@@ -14,8 +14,31 @@ import (
 	"log"
 )
 
-func DataBaseConnect() *gorm.DB {
-	env := godotenv.Load() // Load env file
+// pathオプション構造体
+type PathOptions struct {
+	path string
+}
+
+type option func(*PathOptions)
+
+// pathオプションを設定する関数
+func SetPathOption(path string) option {
+	return func(po *PathOptions) {
+		po.path = path
+	}
+}
+
+func DataBaseConnect(opts ...option) *gorm.DB {
+	// デフォルトパラメータを定義
+	g := &PathOptions{
+		path: ".env",
+	}
+	// ユーザから渡された値だけ上書き
+	for _, opt := range opts {
+		opt(g)
+	}
+
+	env := godotenv.Load(g.path) // Load env file
 	if env != nil {
 		log.Fatal("Error loading .env file")
 	}
